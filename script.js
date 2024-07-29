7c9d07ba0d099a2f07b5464dd30e36ebdf2312a5
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-29 21:24:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+document.getElementById('userForm').addEventListener('submit', (e)=>{
+	e.preventDefault();
+let name=document.getElementById('name').value;
+let age=document.getElementById('age').value;
+function display(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(() => {
+	      if (age >= 18) {
+	        resolve(`Welcome, ${name}. You can vote.`);
+	      } else {
+	        reject(`Oh sorry ${name}. You aren't old enough.`);
+	      }
+    }, 1000);
+	})
+}
+	display()
+	.then(res=>{
+		alert(res);
+	})
+	.catch(err=>{
+		alert(err);
+	})
+});
